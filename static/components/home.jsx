f72d69ef1262c1fd295c33b7514e1a993207609e
vagrant <vagrant@vagrant.vm>
2019-06-16 19:25:48 +0000

    correct a className

--- static/components/home.jsx
+++ static/components/home.jsx
@@ -8,7 +8,7 @@ class Home extends React.Component {
 
     render() {
         return (
-            <div id="carouselExampleControls" className="carousel slide" data-ride="carousel">
+            <div id="carouselControls" className="carousel slide" data-ride="carousel">
                 <div className="carousel-inner">
                     <div className="carousel-item active">
                         <img className="d-block w-100" src="/static/assets/1.png" alt="First slide" />
@@ -20,11 +20,11 @@ class Home extends React.Component {
                         <img className="d-block w-100" src="/static/assets/3.png" alt="Third slide" />
                     </div>
                 </div>
-                <a className="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                <a className="carousel-control-prev" href="#carouselControls" role="button" data-slide="prev">
                     <span className="carousel-control-prev-icon" aria-hidden="true"></span>
                     <span className="sr-only">Previous</span>
                 </a>
-                <a className="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                <a className="carousel-control-next" href="#carouselControls" role="button" data-slide="next">
                     <span className="carousel-control-next-icon" aria-hidden="true"></span>
                     <span className="sr-only">Next</span>
                 </a>
